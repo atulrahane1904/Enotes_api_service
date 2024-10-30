789158a78de5d481671e459991a1c169e0939b05
atul_rahane <atulrahane1904@gmail.com>
2024-10-30 22:07:13 +0530

    initial Commit

---
+++ enotes/src/main/java/com/rahane/enotes/EnotesApplication.java
@@ -0,0 +1,13 @@
+package com.rahane.enotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnotesApplication.class, args);
+	}
+
+}
